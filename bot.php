24a8388ed5e4ed2d231cd70ab10ee3c2874fa597
Narupol Balavarnabha <narupol.b@gmail.com>
2017-08-08 12:39:08 +0700

    Test bot 2

--- bot.php
+++ bot.php
@@ -8,7 +8,7 @@ $headers = array(
     )
 $url = 'https://api.line.me/v1/oauth/verify';
 
-$ch = curl_init($url);  
+$ch = curl_init();  
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch,CURLOPT_URL,$url);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);

522de73d9d33b11b288932676370f8ab47ae8fd5
Narupol Balavarnabha <narupol.b@gmail.com>
2017-08-09 11:15:00 +0700

    Include Laravel

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::get('autoreply','linebotController@autoreply');
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',

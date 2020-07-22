6e0ac0785f53f633b25eafd6d423a50e05460b9f
TRUEDIGITAL\narupol.bal <narupol.b@gmail.com>
2020-07-22 14:10:47 +0700

    Start Laravel - routor

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # naroky-bot
+
+Service Test
+notify : https://narokybot.herokuapp.com/noitfy.php
+autoreply : https://narokybot.herokuapp.com/autoreply.php
+bot : https://narokybot.herokuapp.com/bot.php
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,10 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+Route::get('/' ,function () {
+	return view('welcome');
+})
+/*
 Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
@@ -21,3 +24,4 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+*/

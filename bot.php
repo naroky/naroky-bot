0ce0267c32adb57e0c50e64e1a47dd49444e9494
naroky <narupol.b@gmail.com>
2021-04-09 17:36:40 +0700

    Change Code

--- bot.php
+++ bot.php
@@ -1,23 +1,71 @@
 <?php 
-echo "I am a bot";
+$accessToken = "XeUuwvrwWUW0jEVCRJ/awcN/LSgVw0nrSM8cw/3u6ZTTBMLv9vNEeTeZdvax38qkOkomjZT8Q8yvRLskNXmPotVhZnOJ1BiRK42YBbYr/+tiBzAaH9SwnjGIkpqySjhkVp0RIXhvj5g0KfX6Ie6LgQdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+    
+$content = file_get_contents('php://input');
+$arrayJson = json_decode($content, true);
 
-$tokken = "XeUuwvrwWUW0jEVCRJ/awcN/LSgVw0nrSM8cw/3u6ZTTBMLv9vNEeTeZdvax38qkOkomjZT8Q8yvRLskNXmPotVhZnOJ1BiRK42YBbYr/+tiBzAaH9SwnjGIkpqySjhkVp0RIXhvj5g0KfX6Ie6LgQdB04t89/1O/w1cDnyilFU=";
+$arrayHeader = array();
+$arrayHeader[] = "Content-Type: application/json";
+$arrayHeader[] = "Authorization: Bearer {$accessToken}";
 
-$headers = array(
-    'Authorization: Bearer '.$tokken,
-    );
-$url = 'https://api.line.me/v1/oauth/verify';
-
-$ch = curl_init();  
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch,CURLOPT_URL,$url);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-//  curl_setopt($ch,CURLOPT_HEADER, false); 
-//curl_setopt($ch, CURLOPT_POST, 1);
-	//curl_setopt($ch, CURLOPT_POSTFIELDS,$vars); 
-$output=curl_exec($ch);
- 
-curl_close($ch);
-echo $output;
-
+//รับข้อความจากผู้ใช้
+$message = $arrayJson['events'][0]['message']['text'];
+#ตัวอย่าง Message Type "Text"
+if($message == "สวัสดี"){
+    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+    $arrayPostData['messages'][0]['type'] = "text";
+    $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+    replyMsg($arrayHeader,$arrayPostData);
+}
+#ตัวอย่าง Message Type "Sticker"
+else if($message == "ฝันดี"){
+    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+    $arrayPostData['messages'][0]['type'] = "sticker";
+    $arrayPostData['messages'][0]['packageId'] = "2";
+    $arrayPostData['messages'][0]['stickerId'] = "46";
+    replyMsg($arrayHeader,$arrayPostData);
+}
+#ตัวอย่าง Message Type "Image"
+else if($message == "รูปน้องแมว"){
+    $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
+    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+    $arrayPostData['messages'][0]['type'] = "image";
+    $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
+    $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
+    replyMsg($arrayHeader,$arrayPostData);
+}
+#ตัวอย่าง Message Type "Location"
+else if($message == "พิกัดสยามพารากอน"){
+    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+    $arrayPostData['messages'][0]['type'] = "location";
+    $arrayPostData['messages'][0]['title'] = "สยามพารากอน";
+    $arrayPostData['messages'][0]['address'] =   "13.7465354,100.532752";
+    $arrayPostData['messages'][0]['latitude'] = "13.7465354";
+    $arrayPostData['messages'][0]['longitude'] = "100.532752";
+    replyMsg($arrayHeader,$arrayPostData);
+}
+#ตัวอย่าง Message Type "Text + Sticker ใน 1 ครั้ง"
+else if($message == "ลาก่อน"){
+    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+    $arrayPostData['messages'][0]['type'] = "text";
+    $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
+    $arrayPostData['messages'][1]['type'] = "sticker";
+    $arrayPostData['messages'][1]['packageId'] = "1";
+    $arrayPostData['messages'][1]['stickerId'] = "131";
+    replyMsg($arrayHeader,$arrayPostData);
+}
+function replyMsg($arrayHeader,$arrayPostData){
+    $strUrl = "https://api.line.me/v2/bot/message/reply";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$strUrl);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+    curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($ch);
+    curl_close ($ch);
+}
+exit;
 ?>
